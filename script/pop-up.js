2c919cdd74b6243b6ce6fee2e6268090ce0ff8fe
thunderfreeze <you@example.comyohann.tonnerre@gmail,com>
2019-10-26 17:09:40 +0200

    j'ai fait plein de chose la

--- script/pop-up.js
+++ script/pop-up.js
@@ -1,8 +1,8 @@
 function oui(){
-	document.getElementById('login').setAttribute("class", "style1");
+	document.getElementById('login').setAttribute("class", "login-visible");
 }
 
 
 function non(){
-	document.getElementById('login').setAttribute("class", "style2");
+	document.getElementById('login').setAttribute("class", "login-hidden");
 }
